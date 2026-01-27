ffb1400d8275295e1689c57ecf1ce7333a07b7db
Hady El Fadaly <125579914+hadyelfadaly@users.noreply.github.com>
2026-01-27 01:04:54 +0000

    docker done

--- Module01-Docker-Terraform/Pipelines/ingest_data.py
+++ Module01-Docker-Terraform/Pipelines/ingest_data.py
@@ -2,6 +2,7 @@
 import pandas as pd
 from sqlalchemy import create_engine
 from tqdm.auto import tqdm #to see progress of inserting
+import click
 
 #Fixing data types problem
 dtype = {
@@ -66,7 +67,48 @@ def run():
 
         print("Inserted another chunk")
 
+#another way to write the script is to use click library to pass parameters from command line
+
+@click.command()
+@click.option('--year', default=2021, help='Year of the data to ingest')
+@click.option('--month', default=1, help='Month of the data to ingest')
+@click.option('--pg-user', default='root', help='PostgreSQL user')
+@click.option('--pg-pass', default='root', help='PostgreSQL password')
+@click.option('--pg-host', default='localhost', help='PostgreSQL host')
+@click.option('--pg-port', default=5432, type=int, help='PostgreSQL port')
+@click.option('--pg-db', default='ny_taxi', help='PostgreSQL database name')
+@click.option('--table-name', default='yellow_taxi_data', help='Target table name')
+@click.option('--chunk-size', default=100000, type=int, help='Number of rows per chunk to ingest')
+
+def run2(year, month, pg_user, pg_pass, pg_host, pg_port, pg_db, table_name, chunk_size):
+
+    url = f'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow/yellow_tripdata_{year}-{month:02d}.csv.gz'
+    engine = create_engine(f'postgresql://{pg_user}:{pg_pass}@{pg_host}:{pg_port}/{pg_db}')
+    first = True
+
+    #to divide the data into chunks of equal size so we can inser chunk by chunk
+    df_iter = pd.read_csv(
+        url,
+        dtype=dtype,
+        parse_dates=parse_dates,
+        iterator=True,
+        chunksize=chunk_size
+    )
+
+    for df_chunk in tqdm(df_iter):
+
+        if first:
+
+            df_chunk.head(n=0).to_sql(name=table_name, con=engine, if_exists='replace') #creating table
+
+            first = False
+
+            print("Table created successfully")
+
+        df_chunk.to_sql(name=table_name, con=engine, if_exists='append') #inserting chunk by chunk
+
+        print("Inserted another chunk")
 
 if __name__ == '__main__':
 
-    run()
+    run2()
